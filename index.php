19303365d16cddd47e3de3db7af097bcbbe3b9d9
sergionidis <39002616+sergionidis@users.noreply.github.com>
2021-05-03 11:14:45 +0200

    index con prueba de conexión a DB

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 print `ifconfig`;
+
+#prueba de conexion a mariadb
+mysql_connect(“dbserver”, “root”, “PasswordSecreta”) or die(mysql_error());
+echo “Connected to MySQL<br />”;
+#prueba de conexión a la base de datos.
+mysql_select_db(“testdb”) or die(mysql_error());
+echo “Connected to Database”;
+?>
 ?>
